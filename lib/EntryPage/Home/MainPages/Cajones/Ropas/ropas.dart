0a7f5771e1a510fb206572b4be6799bf2b8bdb2a
danmunozbe <danmunozbe@unal.edu.co>
2022-06-02 21:14:45 -0500

    AGHH

--- lib/EntryPage/Home/MainPages/Cajones/Ropas/ropas.dart
+++ lib/EntryPage/Home/MainPages/Cajones/Ropas/ropas.dart
@@ -1,7 +1,8 @@
 import 'dart:collection';
-
+import 'dart:math';
 import 'package:eco_fit/EntryPage/Home/MainPages/Cajones/Ropas/padres.dart';
 import 'package:eco_fit/EntryPage/Home/MainPages/Cajones/cajon.dart';
+import 'package:eco_fit/Implementaciones/heapsort.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -13,11 +14,15 @@ class DentroCajon extends StatefulWidget {
 }
 
 class _DentroCajonState extends State<DentroCajon> { 
-  
+  //Nombres de las prendas de pruebas 
+  //Puedes cambiarlo para hacer pruebas con nombres aleatoreos
+  //Pero entonces Cambia en la linea "44" lo que esta desdepues de "names:"
+  final List<int> tests=[3,6,2,8,7];
+  int count=0;
   @override
   Widget build(BuildContext context) {
     Padres title = Provider.of<Padres>(context,listen: true);
-    LinkedList<Clothes> prendas= title.padre.cllist;
+    Heap prendas= title.padre.cllist;
     
     return MaterialApp(
       title: 'Material App',
@@ -29,26 +34,53 @@ class _DentroCajonState extends State<DentroCajon> {
           child: inEmpty(list: prendas, 
           ),
         ),
+        floatingActionButton: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            FloatingActionButton(
+              heroTag: "ADD",
+              onPressed: () {
+                //Prenda a agregar
+                Clothes temp= Clothes(name: "${tests[count]}", imgName: title.padre.imgName, cajon: title.padre.name);
+                setState((){
+                  prendas.addToHeap(clothe: temp);
+                  count++;});
+                },
+              child: const Icon(Icons.add) ),
+            FloatingActionButton(
+              heroTag: "SORT",
+              onPressed: () {               
+                setState((){
+                  //Sortea las prendas
+                  prendas.list=prendas.sort(entry: prendas.list);
+                  ;});
+                },
+               child: const Icon(Icons.sort_by_alpha), ),       
+          ],
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+        
       ),
     );
   }
 }
 
-Widget inEmpty({required LinkedList<Clothes> list}) {
+Widget inEmpty({required Heap list}) {
   if (list.isEmpty){
     return const Text("No hay prendas");
   }else{
     return ListView.builder(
-      itemCount: list.length,
+      addAutomaticKeepAlives: false,
+      itemCount: list.size,
       itemBuilder: (BuildContext context, int index) {
         return Card(
           child: Column(
             children: [
               ListTile(
-                leading: Image.asset('assets/imgs/${list.elementAt(index).imgName}.png',
+                leading: Image.asset('assets/imgs/${list.list[index].imgName}.png',
                         fit:BoxFit.cover),
                 title:  Text(
-               list.elementAt(index).name,
+               list.list[index].name,
                   style: const TextStyle(fontSize: 14.0, fontWeight: FontWeight.bold),
           ),
               )
